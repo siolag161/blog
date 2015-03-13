34d42c3ccdcfe473088c40860236bf20734b3444
pdt <thanh.phan@outlook.com>
2015-03-13 23:51:28 +0100

    updated cname

--- settings/prod_settings.py
+++ settings/prod_settings.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__)))
 from base_settings import *
 
 # PRODUCTION CHANGE
-SITEURL = "http://siolag161.github.io/"
+SITEURL = "http://pdt.space/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
